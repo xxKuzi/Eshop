9c2b3762fbb6b3d5c4172f6b06e79d433c4f8668
xxKuzi <jakub.kuzel7@gmail.com>
2024-07-09 20:47:19 +0200

    data to devOrders

--- src/dev/DevOrders.jsx
+++ src/dev/DevOrders.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { db } from "../parts/Base";
 import { useData } from "../parts/Memory.jsx";
 import { collection, getDocs } from "firebase/firestore";
+import { filterProps } from "framer-motion";
 
 export default function DevOrders() {
-  const { profile } = useData();
-  const [data, setData] = useState([
+  const { profile, catalog } = useData();
+  const [orders, setOrders] = useState([
     {
       id: "bagr",
       surname: "false",
@@ -15,17 +16,29 @@ export default function DevOrders() {
       ],
     },
   ]);
+  const [data, setData] = useState([{ productId: "1" }]);
 
   useEffect(() => {
-    console.log(data.products);
     loadOrders(false);
     async function loadOrders() {
       const ordersSnapshot = await getDocs(collection(db, "orders"));
       const ordersArr = ordersSnapshot.docs.map((doc) => ({ ...doc.data() }));
+      const filteredOrders = ordersArr.filter((order) => order.uid === profile.uid);
+      setOrders(filteredOrders);
 
-      const Arr = ordersArr.filter((order) => order.uid === profile.uid);
+      let dataArr = [];
+      filteredOrders.map((order) => {
+        order.products.map((product) => {
+          catalog.map((catalogProduct) => {
+            if (catalogProduct.id === product.id) {
+              dataArr.push({ ...catalogProduct, quantity: product.quantity });
+            }
+          });
+        });
 
-      setData(Arr);
+        setData(dataArr);
+        console.log(dataArr);
+      });
     }
   }, []);
 
@@ -34,26 +47,26 @@ export default function DevOrders() {
       <div className="flex flex-col items-center gap-2 rounded-xl border-4 p-4">
         <p className="headline">Orders</p>
         <div className="mt-2 flex flex-col items-center">
-          {data.map((item, i) => (
+          {orders.map((order, i) => (
             <div key={i} className="flex items-center justify-center gap-2 rounded-lg border-2 border-brand-blue p-2">
               <div className="flex flex-col items-center justify-center">
                 <p className="headline__small">Contact</p>
                 <p className="h-1 w-64 border-2"></p>
-                <p>{item.forename + " " + item.surname}</p>
-                <p>{item.uid}</p>
+                <p>{order.forename + " " + order.surname}</p>
+                <p>{order.uid}</p>
               </div>
 
+              {data.map((product, i) => (
+                <div key={i} className="flex flex-col items-center justify-center">
+                  <p>id: {product.id}</p>
+                  <p>quantity: {product.quantity}</p>
+                  <p>price: {product.price * product.quantity}</p>
+                  <img className="h-16" src={product.images[0].url} />
+                </div>
+              ))}
+              <p>final price: {data.reduce((sum, item) => sum + item.price * item.quantity, 0)}</p>
               <div className="flex flex-col items-center justify-center">
-                0
-                {item.products.map((product) => (
-                  <div>
-                    <p>id: {product.id}</p>
-                    <p>quantity: {product.quantity}</p>
-                  </div>
-                ))}
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <p className={"font-bold " + (item.state === "success" ? "text-green-500" : "text-red-600")}>{item.state}</p>
+                <p className={"font-bold " + (order.state === "success" ? "text-green-500" : "text-red-600")}>{order.state}</p>
                 <p>Payment id:</p>
               </div>
             </div>
