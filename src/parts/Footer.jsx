72d93f36c543cd5e9114927b52639ed0b2f1ca09
xxKuzi <jakub.kuzel7@gmail.com>
2025-10-04 03:41:52 +0200

    better footer

--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <div className="mt-[100px] flex w-full items-center justify-between bg-gray-800 px-4 py-2">
       <div className="flex w-64 justify-start">
-        <img className="h-[100px] rounded-lg bg-white" src="logo.png" />
+        <img className="h-[60px] rounded-lg bg-white" src="logo.png" />
       </div>
 
-      <div className=" flex w-64 flex-col items-center justify-center text-white">
+      <div className=" flex w-64 items-center justify-center gap-8 text-nowrap text-white">
         <a className="hover:underline" href="/">
           Domů
         </a>
